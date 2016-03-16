9e2907d62007d7c70ae5ceb1825d5b7a98c21dbe
Gokulnath Murugesan <gokulnathmurugesan@gmail.com>
2016-03-16 10:36:33 -0400

    modified the identity server to work as saml client

--- src/Libraries/Thinktecture.IdentityServer.Protocols/WSFederation/WSFederationResult.cs
+++ src/Libraries/Thinktecture.IdentityServer.Protocols/WSFederation/WSFederationResult.cs
@@ -4,6 +4,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Protocols.WSTrust;
 using System.IdentityModel.Services;
 using System.Web.Mvc;
@@ -14,15 +15,16 @@ namespace Thinktecture.IdentityServer.Protocols.WSFederation
     {
         public WSFederationResult(SignInResponseMessage message, bool requireSsl)
         {
-            if (requireSsl)
-            {
-                if (message.BaseUri.Scheme != Uri.UriSchemeHttps)
-                {
-                    Tracing.Error(Resources.WSFederation.WSFederationResult.ReturnUrlMustBeSslException);
-                    throw new InvalidRequestException(Resources.WSFederation.WSFederationResult.ReturnUrlMustBeSslException);
-                }
-            }
+            //if (requireSsl)
+            //{
+            //    if (message.BaseUri.Scheme != Uri.UriSchemeHttps)
+            //    {
+            //        Tracing.Error(Resources.WSFederation.WSFederationResult.ReturnUrlMustBeSslException);
+            //        throw new InvalidRequestException(Resources.WSFederation.WSFederationResult.ReturnUrlMustBeSslException);
+            //    }
+            //}
 
+            message.Parameters.Add(new KeyValuePair<string, string>("tokentype", "saml"));
             Content = message.WriteFormPost();
         }
     }
